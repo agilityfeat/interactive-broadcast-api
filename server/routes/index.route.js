e2789e9f0e796d5f3bf45fbf5f39b07f8eafbf91
deviantfero <fmorataya.04@gmail.com>
2018-11-21 02:56:26 -0600

    add new /viewer route to route.js

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -2,11 +2,13 @@ import express from 'express';
 import adminRoutes from './admin.route';
 import authRoutes from './auth.route';
 import eventRoutes from './event.route';
+import viewerRoutes from './viewer.route';
 
 const router = express.Router(); // eslint-disable-line new-cap
 // mount admin routes at /admin
 router.use('/admin', adminRoutes);
 router.use('/auth', authRoutes);
 router.use('/event', eventRoutes);
+router.use('/viewer', viewerRoutes);
 
 export default router;
